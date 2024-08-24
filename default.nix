1d2fed18d4d0729cc6878b8b2a813bbd6f208765
antangelo <contact@antangelo.com>
2024-08-24 01:47:51 -0400

    cli: Package with nix

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { } }: {
+  cli = let
+    manifest = (pkgs.lib.importTOML ./Cargo.toml).workspace.package;
+  in pkgs.rustPlatform.buildRustPackage {
+    version = manifest.version;
+
+    src = pkgs.lib.cleanSource ./.;
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+    };
+
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+    pname = "xdvdfs";
+    cargoBuildFlags = "--package xdvdfs-cli";
+
+    cargoTestFlags = [
+      # Only run checks for CLI and its dependency
+      # We don't want to run checks on -web or -desktop here
+      "--package xdvdfs-cli"
+      "--package xdvdfs"
+    ];
+  };
+}
