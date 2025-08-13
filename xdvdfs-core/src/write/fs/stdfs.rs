a6e12e159083abe83479cdd960d94b07684cb7c7
antangelo <contact@antangelo.com>
2025-08-12 23:29:27 -0400

    core: Add stdfs tests (#164)

--- xdvdfs-core/src/write/fs/stdfs.rs
+++ xdvdfs-core/src/write/fs/stdfs.rs
@@ -124,7 +124,7 @@ impl FilesystemCopier<[u8]> for StdFilesystem {
 
         let dest = &mut dest[output_offset..(output_offset + size)];
         let bytes_read = Read::read(&mut file, dest)?;
-        dest[(output_offset + bytes_read)..].fill(0);
+        dest[bytes_read..].fill(0);
         Ok(<[u8]>::len(dest) as u64)
     }
 }
@@ -143,5 +143,150 @@ impl FilesystemCopier<NullBlockDevice> for StdFilesystem {
     ) -> Result<u64, core::convert::Infallible> {
         dest.write_size_adjustment(output_offset, size);
         Ok(size)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use alloc::vec::Vec;
+    use std::io::{Cursor, Write};
+
+    use futures::executor::block_on;
+
+    use crate::{
+        blockdev::NullBlockDevice,
+        write::fs::{FileEntry, FileType, FilesystemCopier, FilesystemHierarchy},
+    };
+
+    use super::StdFilesystem;
+
+    #[test]
+    fn test_write_stdfs_read_dir() {
+        let tempdir = tempfile::tempdir().expect("Failed to create tempdir");
+
+        std::fs::create_dir(tempdir.path().join("subdir")).expect("Failed to create dir direntry");
+        std::fs::create_dir(tempdir.path().join("subdir/entry"))
+            .expect("Failed to create dir direntry");
+
+        {
+            let mut file = std::fs::File::create(tempdir.path().join("file"))
+                .expect("Failed to create file direntry");
+            file.write_all(b"Hello World")
+                .expect("Failed to write to file entry");
+        }
+
+        let mut stdfs = StdFilesystem::create(tempdir.path());
+
+        let mut result = block_on(stdfs.read_dir("/".into())).expect("read_dir should succeed");
+
+        // Filesystem ordering is non-deterministic,
+        // so sort the records by name.
+        result.sort();
+        assert_eq!(
+            result,
+            &[
+                FileEntry {
+                    name: "file".into(),
+                    file_type: FileType::File,
+                    len: 11,
+                },
+                FileEntry {
+                    name: "subdir".into(),
+                    file_type: FileType::Directory,
+                    len: 0,
+                },
+            ],
+        );
+
+        let result = block_on(stdfs.read_dir("/subdir".into())).expect("read_dir should succeed");
+        assert_eq!(
+            result,
+            &[FileEntry {
+                name: "entry".into(),
+                file_type: FileType::Directory,
+                len: 0,
+            },],
+        );
+    }
+
+    #[test]
+    fn test_write_stdfs_io_write_copier() {
+        let tempdir = tempfile::tempdir().expect("Failed to create tempdir");
+
+        std::fs::create_dir(tempdir.path().join("subdir")).expect("Failed to create dir direntry");
+
+        {
+            let mut file = std::fs::File::create(tempdir.path().join("subdir/file"))
+                .expect("Failed to create file direntry");
+            file.write_all(b"Hello World")
+                .expect("Failed to write to file entry");
+        }
+
+        let mut stdfs = StdFilesystem::create(tempdir.path());
+        let mut cursor = Cursor::new(Vec::<u8>::new());
+
+        let result = block_on(stdfs.copy_file_in(
+            "subdir/file".into(),
+            &mut cursor,
+            /*input_offset=*/ 1,
+            /*output_offset=*/ 2,
+            /*size=*/ 10,
+        ));
+        assert!(result.is_ok_and(|len| len == 10));
+        assert_eq!(cursor.get_ref(), b"\0\0ello World");
+    }
+
+    #[test]
+    fn test_write_stdfs_byte_slice_copier() {
+        let tempdir = tempfile::tempdir().expect("Failed to create tempdir");
+
+        std::fs::create_dir(tempdir.path().join("subdir")).expect("Failed to create dir direntry");
+
+        {
+            let mut file = std::fs::File::create(tempdir.path().join("subdir/file"))
+                .expect("Failed to create file direntry");
+            file.write_all(b"Hello World")
+                .expect("Failed to write to file entry");
+        }
+
+        let mut stdfs = StdFilesystem::create(tempdir.path());
+        let mut buffer = [0u8; 13];
+
+        let result = block_on(stdfs.copy_file_in(
+            "subdir/file".into(),
+            buffer.as_mut_slice(),
+            /*input_offset=*/ 1,
+            /*output_offset=*/ 2,
+            /*size=*/ 10,
+        ));
+        assert!(result.is_ok_and(|len| len == 10));
+        assert_eq!(&buffer, b"\0\0ello World\0");
+    }
+
+    #[test]
+    fn test_write_stdfs_null_copier() {
+        let tempdir = tempfile::tempdir().expect("Failed to create tempdir");
+
+        std::fs::create_dir(tempdir.path().join("subdir")).expect("Failed to create dir direntry");
+
+        {
+            let mut file = std::fs::File::create(tempdir.path().join("subdir/file"))
+                .expect("Failed to create file direntry");
+            file.write_all(b"Hello World")
+                .expect("Failed to write to file entry");
+        }
+
+        let mut stdfs = StdFilesystem::create(tempdir.path());
+        let mut nullbd = NullBlockDevice::default();
+
+        let result = block_on(stdfs.copy_file_in(
+            "subdir/file".into(),
+            &mut nullbd,
+            /*input_offset=*/ 1,
+            /*output_offset=*/ 2,
+            /*size=*/ 10,
+        ));
+        assert!(result.is_ok_and(|len| len == 10));
+        assert_eq!(nullbd.len_blocking(), 12);
     }
 }
